c0b52c4aacc3138e5cbafe2fecdc0987e29d4336
StamFrank <eugenii.strunets@gmail.com>
2025-01-07 22:27:23 +0300

    lesson 9 kotlin interpretation

--- app/src/main/java/ru/success/road_to_success/MainActivity.kt
+++ app/src/main/java/ru/success/road_to_success/MainActivity.kt
@@ -1,16 +1,31 @@
 package ru.success.road_to_success
 
+
+
+
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val myTextView = findViewById<View>(R.id.myText) as TextView
+        myTextView.setText("New text in TextView");
+        val myBtn = findViewById<View>(R.id.myBtn) as Button
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
+        val myChb = findViewById<View>(R.id.myChb) as CheckBox
+        myChb.isChecked = true
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
