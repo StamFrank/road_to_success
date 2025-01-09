7066209ec368dcf07b1ac7848fd9bf50a375a8b7
StamFrank <eugenii.strunets@gmail.com>
2025-01-10 02:12:11 +0300

    Resources has been created and edited.

--- app/src/main/java/ru/success/road_to_success/MainActivity.kt
+++ app/src/main/java/ru/success/road_to_success/MainActivity.kt
@@ -1,11 +1,9 @@
 package ru.success.road_to_success
 
-
-
-
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,32 +12,19 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
-    var tvOut: TextView? = null
-    var btnOk: Button? = null
-    var btnCancel: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        tvOut = findViewById(R.id.tvOut)
-        btnOk = findViewById(R.id.btnOk)
-        btnCancel = findViewById(R.id.btnCancel)
+        val llBottom = findViewById<View>(R.id.llBottom) as LinearLayout
+        val tvBottom = findViewById<View>(R.id.tvBottom) as TextView
+        val btnBottom = findViewById<View>(R.id.btnBottom) as Button
 
-        val oclBtnOk = View.OnClickListener {
-
-            tvOut?.setText("Нажата кнопка ОК");
-        }
-
-        btnOk?.setOnClickListener(oclBtnOk);
-
-        val oclBtnCancel = object : View.OnClickListener {
-            override fun onClick(v: View?) { // Меняем текст в TextView (tvOut)
-                tvOut!!.text = "Нажата кнопка Cancel"
-            }
-        }
-
-        btnCancel?.setOnClickListener(oclBtnCancel);
+        llBottom.setBackgroundResource(R.color.llBottomColor)
+        tvBottom.setText(R.string.tvBottomText)
+        btnBottom.setText(R.string.btnBottomText)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
