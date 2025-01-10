c8d38db485d9e2b2e1d061f3928e9c857ceaccc7
StamFrank <eugenii.strunets@gmail.com>
2025-01-11 01:58:07 +0300

    Cheched-menu has been added and improved.

--- app/src/main/java/ru/success/road_to_success/MainActivity.kt
+++ app/src/main/java/ru/success/road_to_success/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.success.road_to_success
 
 
 
+
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -35,15 +36,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean { // TODO Auto-generated method stub
-        // добавляем пункты меню
-        menu.add(0, 1, 0, "add")
-        menu.add(0, 2, 0, "edit")
-        menu.add(0, 3, 3, "delete")
-        menu.add(1, 4, 1, "copy")
-        menu.add(1, 5, 2, "paste")
-        menu.add(1, 6, 4, "exit")
-
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mymenu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
