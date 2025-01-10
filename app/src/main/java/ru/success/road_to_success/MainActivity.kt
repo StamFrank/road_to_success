b603191fa1fce6eb84511aef5179457f796f30fd
StamFrank <eugenii.strunets@gmail.com>
2025-01-10 03:31:14 +0300

    Log filter, Toast messages has been added.

--- app/src/main/java/ru/success/road_to_success/MainActivity.kt
+++ app/src/main/java/ru/success/road_to_success/MainActivity.kt
@@ -1,12 +1,11 @@
 package ru.success.road_to_success
 
-
-
-
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,37 +13,50 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
-    var tvOut: TextView? = null
-    var btnOk: Button? = null
-    var btnCancel: Button? = null
+
+    lateinit var tvOut: TextView
+    lateinit var btnOk: Button
+    lateinit var btnCancel: Button
+
+    val TAG: String = "myLogs"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+        Log.d(TAG, "найдем View-элементы");
         tvOut = findViewById(R.id.tvOut)
         btnOk = findViewById(R.id.btnOk)
         btnCancel = findViewById(R.id.btnCancel)
-
-        val oclBtnOk = View.OnClickListener {
-
-            tvOut?.setText("Нажата кнопка ОК");
-        }
-
-        btnOk?.setOnClickListener(oclBtnOk);
-
-        val oclBtnCancel = object : View.OnClickListener {
-            override fun onClick(v: View?) { // Меняем текст в TextView (tvOut)
-                tvOut!!.text = "Нажата кнопка Cancel"
-            }
-        }
-
-        btnCancel?.setOnClickListener(oclBtnCancel);
-
+        Log.d(TAG, "присваиваем обработчик кнопкам");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    fun onClickStart(v: View?) {
+        Log.d(TAG, "по id определяем кнопку, вызвавшую этот обработчик");
+        if (v != null) {
+            when (v.id) {
+
+                R.id.btnOk     -> {      // кнопка ОК
+                    tvOut.text = "Нажата кнопка ОК"
+                    Log.d(TAG, "кнопка ОК")
+                    Toast.makeText(this, "Нажата кнопка ОК", Toast.LENGTH_LONG).show()
+                }
+
+                R.id.btnCancel -> {       // кнопка Cancel
+                    tvOut.text = "Нажата кнопка Cancel"
+                    Log.d(TAG, "кнопка Cancel")
+                    Toast.makeText(this, "Нажата кнопка Cancel", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+
 }
