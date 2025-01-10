bde2832943fe662a6d6aa394d8cf304d2c030674
StamFrank <eugenii.strunets@gmail.com>
2025-01-11 02:35:20 +0300

    Context menu has been been created.

--- app/src/main/java/ru/success/road_to_success/MainActivity.kt
+++ app/src/main/java/ru/success/road_to_success/MainActivity.kt
@@ -3,9 +3,12 @@ package ru.success.road_to_success
 
 
 
+
+import android.graphics.Color
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,37 +17,94 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
-    var tvOut: TextView? = null
-    var btnOk: Button? = null
-    var btnCancel: Button? = null
+
+    val MENU_COLOR_RED: Int = 1
+    val MENU_COLOR_GREEN: Int = 2
+    val MENU_COLOR_BLUE: Int = 3
+
+    val MENU_SIZE_22: Int = 4
+    val MENU_SIZE_26: Int = 5
+    val MENU_SIZE_30: Int = 6
+
+    lateinit var tvSize: TextView
+    lateinit var tvColor: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        tvOut = findViewById(R.id.tvOut)
-        btnOk = findViewById(R.id.btnOk)
-        btnCancel = findViewById(R.id.btnCancel)
+        tvColor = findViewById(R.id.tvColor);
+        tvSize = findViewById(R.id.tvSize);
 
-        val oclBtnOk = View.OnClickListener {
+        // для tvColor и tvSize необходимо создавать контекстное меню
+        registerForContextMenu(tvColor);
+        registerForContextMenu(tvSize);
 
-            tvOut?.setText("Нажата кнопка ОК");
-        }
-
-        btnOk?.setOnClickListener(oclBtnOk);
-
-        val oclBtnCancel = object : View.OnClickListener {
-            override fun onClick(v: View?) { // Меняем текст в TextView (tvOut)
-                tvOut!!.text = "Нажата кнопка Cancel"
-            }
-        }
-
-        btnCancel?.setOnClickListener(oclBtnCancel);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        when (v!!.id) {
+            R.id.tvColor -> {
+                menu.add(0, MENU_COLOR_RED, 0, "Red")
+                menu.add(0, MENU_COLOR_GREEN, 0, "Green")
+                menu.add(0, MENU_COLOR_BLUE, 0, "Blue")
+            }
+
+            R.id.tvSize  -> {
+                menu.add(0, MENU_SIZE_22, 0, "22")
+                menu.add(0, MENU_SIZE_26, 0, "26")
+                menu.add(0, MENU_SIZE_30, 0, "30")
+            }
+        }
     }
-}
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            MENU_COLOR_RED   -> {
+                tvColor.setTextColor(Color.RED)
+                tvColor.text = "Text color = red"
+            }
+
+            MENU_COLOR_GREEN -> {
+                tvColor.setTextColor(Color.GREEN)
+                tvColor.text = "Text color = green"
+            }
+
+            MENU_COLOR_BLUE  -> {
+                tvColor.setTextColor(Color.BLUE)
+                tvColor.text = "Text color = blue"
+            }
+
+            MENU_SIZE_22     -> {
+                tvSize.textSize = 22f
+                tvSize.text = "Text size = 22"
+            }
+
+            MENU_SIZE_26     -> {
+                tvSize.textSize = 26f
+                tvSize.text = "Text size = 26"
+            }
+
+            MENU_SIZE_30     -> {
+                tvSize.textSize = 30f
+                tvSize.text = "Text size = 30"
+            }
+        }
+        return super.onContextItemSelected(item)
+
+    }
+
+
+    }
