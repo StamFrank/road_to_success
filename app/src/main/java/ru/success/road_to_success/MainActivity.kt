4785c0e7ba4f073bdd98c92fc187a92ba0458b7b
StamFrank <eugenii.strunets@gmail.com>
2025-01-19 03:40:47 +0300

    Calculator with nullcheck division has been realized.

--- app/src/main/java/ru/success/road_to_success/MainActivity.kt
+++ app/src/main/java/ru/success/road_to_success/MainActivity.kt
@@ -1,50 +1,130 @@
 package ru.success.road_to_success
 
-
-
-
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 
-class MainActivity : AppCompatActivity() {
-    var tvOut: TextView? = null
-    var btnOk: Button? = null
-    var btnCancel: Button? = null
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    val MENU_RESET_ID: Int = 1
+    val MENU_QUIT_ID: Int = 2
+
+    lateinit var etNum1: EditText
+    lateinit var etNum2: EditText
+
+    lateinit var btnAdd: Button
+    lateinit var btnSub: Button
+    lateinit var btnMult: Button
+    lateinit var btnDiv: Button
+
+    lateinit var tvResult: TextView
+
+    var oper: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        tvOut = findViewById(R.id.tvOut)
-        btnOk = findViewById(R.id.btnOk)
-        btnCancel = findViewById(R.id.btnCancel)
+        etNum1 = findViewById(R.id.etNum1)
+        etNum2 = findViewById(R.id.etNum2)
 
-        val oclBtnOk = View.OnClickListener {
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSub = findViewById(R.id.btnSub)
+        btnMult = findViewById(R.id.btnMult)
+        btnDiv = findViewById(R.id.btnDiv)
 
-            tvOut?.setText("Нажата кнопка ОК");
-        }
+        tvResult = findViewById(R.id.tvResult)
 
-        btnOk?.setOnClickListener(oclBtnOk);
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMult.setOnClickListener(this)
+        btnDiv.setOnClickListener(this)
 
-        val oclBtnCancel = object : View.OnClickListener {
-            override fun onClick(v: View?) { // Меняем текст в TextView (tvOut)
-                tvOut!!.text = "Нажата кнопка Cancel"
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menu!!.add(0, MENU_RESET_ID, 0, "Reset");
+        menu.add(0, MENU_QUIT_ID, 0, "Quit");
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            MENU_RESET_ID -> {
+                etNum1.setText("")
+                etNum2.setText("")
+            }
+            MENU_QUIT_ID -> {
+                finish()
             }
         }
+        return super.onOptionsItemSelected(item)
+    }
 
-        btnCancel?.setOnClickListener(oclBtnCancel);
+    override fun onClick(v: View?) {
+        var num1 = 0f
+        var num11: String? = null
+        var num2 = 0f
+        var num22: String? = null
+        var result = 0f
+        var nChk = "0"
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        if (TextUtils.isEmpty(etNum1.text.toString())
+            || TextUtils.isEmpty(etNum2.text.toString())) {
+            return
         }
+        num11 = etNum1.text.toString()
+        num1 = num11.toFloat()
+        num22 = etNum2.text.toString()
+        num2 = num22.toFloat()
+
+        when (v!!.id) {
+            R.id.btnAdd  -> {
+                oper = "+"
+                result = num1 + num2
+            }
+
+            R.id.btnSub  -> {
+                oper = "-"
+                result = num1 - num2
+            }
+
+            R.id.btnMult -> {
+                oper = "*"
+                result = num1 * num2
+            }
+
+            R.id.btnDiv  -> {
+
+                oper = "/"
+
+                result = num1 / num2
+
+            }
+
+            else         -> {}
+
+        }
+
+        if (num22 == nChk){
+            tvResult.text = "Произведено деление на ноль"
+        }
+        else {
+            tvResult.text = "$num1 $oper $num2 = $result"
+        }
+
     }
 }
