bd0c50ee9788d534a48d60a0a2ab735abc45b499
StamFrank <eugenii.strunets@gmail.com>
2025-01-25 20:16:39 +0300

    Data base with Preferences has been realized.

--- app/src/main/java/ru/success/road_to_success/MainActivity.kt
+++ app/src/main/java/ru/success/road_to_success/MainActivity.kt
@@ -3,48 +3,89 @@ package ru.success.road_to_success
 
 
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 
-class MainActivity : AppCompatActivity() {
-    var tvOut: TextView? = null
-    var btnOk: Button? = null
-    var btnCancel: Button? = null
+class MainActivity : AppCompatActivity(), OnClickListener{
+
+    lateinit var etText: EditText
+    lateinit var btnSave: Button
+    lateinit var btnLoad: Button
+    lateinit var spinner: Spinner
+    lateinit var sPref: SharedPreferences
+
+    val SAVED_TEXT: String = "saved_text"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        spinner = findViewById(R.id.spinner)
 
-        tvOut = findViewById(R.id.tvOut)
-        btnOk = findViewById(R.id.btnOk)
-        btnCancel = findViewById(R.id.btnCancel)
+        etText = findViewById(R.id.etText)
 
-        val oclBtnOk = View.OnClickListener {
+        btnSave = findViewById(R.id.btnSave)
+        btnSave.setOnClickListener(this)
 
-            tvOut?.setText("Нажата кнопка ОК");
-        }
+        btnLoad = findViewById(R.id.btnLoad)
+        btnLoad.setOnClickListener(this)
 
-        btnOk?.setOnClickListener(oclBtnOk);
 
-        val oclBtnCancel = object : View.OnClickListener {
-            override fun onClick(v: View?) { // Меняем текст в TextView (tvOut)
-                tvOut!!.text = "Нажата кнопка Cancel"
+
+
+
+    }
+    override fun onClick(v: View?) {
+
+        when (v!!.id){
+            R.id.btnSave -> {
+                saveText()
+            //    Toast.makeText(this, spinner.selectedItemId.toString(), Toast.LENGTH_SHORT).show()
+
+            }
+            R.id.btnLoad -> {
+                loadText()
             }
         }
 
-        btnCancel?.setOnClickListener(oclBtnCancel);
+    }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    private fun loadText() {
+        sPref = getPreferences(MODE_PRIVATE)
+        val savedText = sPref.getString(SAVED_TEXT, "")
+        etText.setText(savedText)
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun saveText() {
+        sPref = getPreferences(MODE_PRIVATE)
+        val ed = sPref.edit()
+        ed.putString(SAVED_TEXT, etText.text.toString())
+        ed.apply()
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        when (spinner.selectedItemId.toString())
+        {
+            "1" -> {
+                saveText()
+            }
         }
+
     }
 }
