8470ef0ddc769a40349b180f2810ad4d74e97921
vladislav <vyoncm@gmail.com>
2025-02-06 22:52:22 +0300

    Main activity code refactoring.

--- app/src/main/java/ru/success/road_to_success/MainActivity.kt
+++ app/src/main/java/ru/success/road_to_success/MainActivity.kt
@@ -1,4 +1,5 @@
 package ru.success.road_to_success
+
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
@@ -10,175 +11,224 @@ import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity(), OnClickListener {
+    private lateinit var btnAdd: Button
+    private lateinit var btnRead: Button
+    private lateinit var btnClear: Button
+    private lateinit var btnDel: Button
+    private lateinit var btnUpd: Button
+    private lateinit var etID: EditText
+    private lateinit var etName: EditText
+    private lateinit var etEmail: EditText
 
-    val LOG_TAG: String = "myLogs"
-    lateinit var btnAdd: Button
-    lateinit var btnRead: Button
-    lateinit var btnClear: Button
-    lateinit var btnDel: Button
-    lateinit var btnUpd: Button
-    lateinit var etID: EditText
-    lateinit var etName: EditText
-    lateinit var etEmail: EditText
-    lateinit var listAll: TextView
-
-    var dbHelper: DBHelper? = null
+    private lateinit var dbHelper: DBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        initUI()
+        initDB()
+    }
 
+
+    private fun initUI() {
         etID = findViewById(R.id.etID)
-        etID.setOnClickListener(this)
 
         btnDel = findViewById(R.id.btnDel)
-        btnDel.setOnClickListener(this)
+        btnDel.setOnClickListener { handleOnBtnDeleteClicked() }
 
         btnUpd = findViewById(R.id.btnUpd)
-        btnUpd.setOnClickListener(this)
+        btnUpd.setOnClickListener { handleOnBtnUpdateClicked() }
 
-        btnAdd = findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(this);
+        btnAdd = findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener { handleOnBtnAddClicked() }
 
-//        listAll = findViewById(R.id.listAll)
+        btnRead = findViewById(R.id.btnRead)
+        btnRead.setOnClickListener { handleOnBtnReadClicked() }
 
-        btnRead = findViewById(R.id.btnRead);
-        btnRead.setOnClickListener(this);
+        btnClear = findViewById(R.id.btnClear)
+        btnClear.setOnClickListener { handleOnBtnClearClicked() }
 
-        btnClear = findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(this);
+        etName = findViewById(R.id.etName)
+        etEmail = findViewById(R.id.etEmail)
+    }
 
-        etName = findViewById(R.id.etName);
-        etEmail = findViewById(R.id.etEmail);
-
-        // создаем объект для создания и управления версиями БД
-        dbHelper = DBHelper(this);
-
+    private fun initDB() {
+        dbHelper = DBHelper(this)
     }
 
 
-    override fun onClick(v: View?) {
+    private fun writeToDB(
+        operation: (ContentValues, SQLiteDatabase) -> Unit
+    ) {
+        try {
+            operation(
+                ContentValues(),
+                dbHelper.writableDatabase
+            )
+        } catch (e: Exception) {
+            Log.d(LOG_TAG, "unable to write to DB, message: ${e.message}")
+        } finally {
+            dbHelper.close()
+        }
+    }
+
+    private fun readFromDB(
+        operation: (SQLiteDatabase) -> Unit
+    ) {
+        try {
+            operation(
+                dbHelper.writableDatabase
+            )
+        } catch (e: Exception) {
+            Log.d(LOG_TAG, "unable to read from DB, message: ${e.message}")
+        } finally {
+            dbHelper.close()
+        }
+    }
 
 
-        // создаем объект для данных
-        val cv = ContentValues()
-
-
-        // получаем данные из полей ввода
+    private fun handleOnBtnAddClicked() {
         val name = etName.text.toString()
         val email = etEmail.text.toString()
-        val id = etID.text.toString()
+        if(name.isNotEmpty() && email.isNotEmpty()) {
+            writeToDB { contentValues, dataBase ->
+                contentValues.put(DB_FIELD_NAME, name)
+                contentValues.put(DB_FIELD_EMAIL, email)
 
+                dataBase.insert(TABLE_NAME, null, contentValues)
 
-        // подключаемся к БД
-        val db = dbHelper!!.writableDatabase
-
-
-        when (v!!.id) {
-            R.id.btnAdd   -> {
-                Log.d(LOG_TAG, "--- Insert in mytable: ---")
-
-                // подготовим данные для вставки в виде пар: наименование столбца - значение
-                cv.put("name", name)
-                cv.put("email", email) // вставляем запись и получаем ее ID
-                val rowID = db.insert("mytable", null, cv)
-                Log.d(LOG_TAG, "row inserted, ID = $rowID")
-                etName.text=null
-                etEmail.text=null
+                etName.text = null
+                etEmail.text = null
             }
+        } else {
+            Log.d(LOG_TAG, "input fields are filled in incorrectly")
+        }
+    }
 
-            R.id.btnRead  -> {
-                var listik = ""
-                Log.d(
-                    LOG_TAG, "--- Rows in mytable: ---"
-                ) // делаем запрос всех данных из таблицы mytable, получаем Cursor
-                val c = db.query("mytable", null, null, null, null, null, null)
+    private fun handleOnBtnReadClicked() {
+        readFromDB { dataBase ->
+            var navArgs = "List of rows: \n"
+            val cursor = dataBase.query(
+                TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+            )
 
+            val idColIndex = cursor.getColumnIndex(DB_FIELD_ID)
+            val nameColIndex = cursor.getColumnIndex(DB_FIELD_NAME)
+            val emailColIndex = cursor.getColumnIndex(DB_FIELD_EMAIL)
 
-                // ставим позицию курсора на первую строку выборки
-                // если в выборке нет строк, вернется false
-                if (c.moveToFirst()) { // определяем номера столбцов по имени в выборке
-                    listik = "List of rows: \n"
-                    val idColIndex = c.getColumnIndex("id")
-                    val nameColIndex = c.getColumnIndex("name")
-                    val emailColIndex = c.getColumnIndex("email")
+            while (cursor.moveToNext()) {
+                val id = cursor.getInt(idColIndex)
+                val name = cursor.getString(nameColIndex)
+                val email = cursor.getString(emailColIndex)
 
-                    do { // получаем значения по номерам столбцов и пишем все в лог
-                        Log.d(
-                            LOG_TAG,
-                            "ID = " + c.getInt(idColIndex) + ", name = " + c.getString(nameColIndex) + ", email = " + c.getString(
-                                emailColIndex
-                            )
-                        ) // переход на следующую строку
-                        // а если следующей нет (текущая - последняя), то false - выходим из цикла
-                        listik += ("\nID = " + c.getInt(idColIndex) + ", name = " + c.getString(nameColIndex) + ", email = " + c.getString(
-                            emailColIndex))
-                    } while (c.moveToNext())
-                } else Log.d(LOG_TAG, "0 rows")
-                c.close()
-                val intent = Intent(this, MainActivity2::class.java)
-                intent.putExtra("SendListik", listik)
-                startActivity(intent)
+                val currentRow = "$DB_FIELD_ID = $id, $DB_FIELD_NAME = $name, $DB_FIELD_EMAIL = $email"
 
+                Log.d(LOG_TAG, currentRow)
+
+                navArgs += "\n $currentRow"
             }
+            cursor.close()
 
-            R.id.btnUpd -> {
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra(NAVIGATION_INTENT, navArgs)
+            startActivity(intent)
+        }
+    }
 
-                if((name != "") && (email != "")) {
+    private fun handleOnBtnUpdateClicked() {
+        writeToDB { contentValues, dataBase ->
+            val name = etName.text.toString()
+            val email = etEmail.text.toString()
+            val id = etID.text.toString()
 
-                    Log.d(LOG_TAG, "--- Update mytable: ---")
-                    cv.put("name", name)
-                    cv.put("email", email)
-                    val updCount = db.update("mytable", cv, "id = ?", arrayOf(id))
-                    Log.d(LOG_TAG, "updated rows count = $updCount")
-                }
-
-
-            }
-
-            R.id.btnDel -> {
-
-                Log.d(LOG_TAG, "--- Delete from mytable: ---");
-                val delCount = db.delete("mytable", "id = $id", null)
-                Log.d(LOG_TAG, "deleted rows count = $delCount")
-
-            }
-
-            R.id.btnClear -> {
-                Log.d(LOG_TAG, "--- Clear mytable: ---") // удаляем все записи
-                val clearCount = db.delete("mytable", null, null)
-                Log.d(LOG_TAG, "deleted rows count = $clearCount")
-            }
-            R.id.etID -> {
-                etID.setText("")
+            if (name.isEmpty() || email.isEmpty() || id.isEmpty()) {
+                Log.d(LOG_TAG, "input fields are filled in incorrectly")
+            } else {
+                contentValues.put(DB_FIELD_NAME, name)
+                contentValues.put(DB_FIELD_EMAIL, email)
+                val updCount = dataBase.update(
+                    TABLE_NAME,
+                    contentValues,
+                    "$DB_FIELD_ID = ?",
+                    arrayOf(id)
+                )
+                Log.d(LOG_TAG, "updated rows count = $updCount")
             }
         }
+    }
 
-        // закрываем подключение к БД
-        dbHelper!!.close()
+    private fun handleOnBtnDeleteClicked() {
+        writeToDB { _, dataBase ->
+            val id = etID.text.toString()
+            val delCount = dataBase.delete(
+                TABLE_NAME,
+                "$DB_FIELD_ID = $id",
+                null
+            )
+            Log.d(LOG_TAG, "deleted rows count = $delCount")
+        }
+    }
 
-
+    private fun handleOnBtnClearClicked() {
+        writeToDB { _, dataBase ->
+            val delCount = dataBase.delete(
+                TABLE_NAME,
+                null,
+                null
+            )
+            Log.d(LOG_TAG, "deleted rows count = $delCount")
+        }
     }
 
 
-    class DBHelper(context: Context?) : SQLiteOpenHelper(context, "myDB", null, 1) {
+    override fun onClick(p0: View?) {
+
+    }
+
+    class DBHelper(context: Context?) : SQLiteOpenHelper(
+        context,
+        DB_NAME,
+        null,
+        DB_VERSION
+    ) {
         override fun onCreate(db: SQLiteDatabase) {
-//            Log.d(LOG_TAG, "--- onCreate database ---") // создаем таблицу с полями
-            db.execSQL(
-                ("create table mytable (" + "id integer primary key autoincrement," + "name text," + "email text" + ");")
-            )
+            db.execSQL(DB_CREATE_REQUEST)
         }
 
         override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+
         }
     }
 
+    companion object {
+        private const val LOG_TAG: String = "myLogs"
+        private const val TABLE_NAME: String = "mytable"
+        private const val NAVIGATION_INTENT: String = "SendListik"
 
+        private const val DB_NAME: String = "myDB"
+        private const val DB_VERSION: Int = 1
+        private const val DB_FIELD_ID: String = "id"
+        private const val DB_FIELD_NAME: String = "name"
+        private const val DB_FIELD_EMAIL: String = "email"
+
+        private const val DB_CREATE_REQUEST: String =
+            "create table $TABLE_NAME " +
+            "(" +
+            "$DB_FIELD_ID integer primary key autoincrement," +
+            "$DB_FIELD_NAME text," +
+            "$DB_FIELD_EMAIL text" +
+            ");"
+    }
 }
